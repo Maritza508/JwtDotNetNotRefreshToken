1179b7529a9496bad1b2c7d4495bf85870aecd1d
Maritza Ruiz Ospina <maritzaruizospina@gmail.com>
2023-08-13 17:32:44 -0500

    firts commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using DemoJWTNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DemoJWTNetCore.Controllers
+{
+    [ApiController]
+    [Route("usuario")]
+    public class UserController : ControllerBase
+    {
+        public IConfiguration _configuration;
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public dynamic IniciarSesion([FromBody] Object objRequest)
+        {
+            var data = JsonConvert.DeserializeObject<dynamic>(objRequest.ToString());
+            string user = data.nombre.ToString();
+            string password = data.password.ToString();
+
+            Usuario usuario = Usuario.DB().Where(p => p.nombre == user && p.password == password).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return new
+                {
+                    succes = false,
+                    message = "Credenciales incorrectas",
+                    result = ""
+                };
+            }
+
+            var jwt = _configuration.GetSection("Jwt").Get<Jwt>();
+            var claims = new[]
+            {
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sub, jwt.Subject),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                new Claim("id", usuario.idUsuario.ToString()),
+                new Claim("usuario", usuario.nombre)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key));
+            var singIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                jwt.Issuer,
+                jwt.Audience,
+                claims,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: singIn
+                );
+
+            return new
+            {
+                succes = true,
+                message = "exitoo!!",
+                resultToken = new JwtSecurityTokenHandler().WriteToken(token)
+            };
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,49 @@
+﻿using DemoJWTNetCore.Models;
+
+namespace DemoJWTNetCore.Models
+{
+    public class Usuario
+    {
+        public string idUsuario { get; set; }
+        public string nombre { get; set; }
+        public string password { get; set; }
+        public string rol { get; set; }
+
+        public static List<Usuario> DB()
+        {
+            var list = new List<Usuario>()
+            {
+                new Usuario
+                {
+                    idUsuario = "1",
+                    nombre = "Esteban",
+                    password = "3",
+                    rol = "empleado",
+                },
+                new Usuario
+                {
+                    idUsuario = "2",
+                    nombre = "Sara",
+                    password = "3",
+                    rol = "empleado",
+                },
+                new Usuario
+                {
+                    idUsuario = "3",
+                    nombre = "Nieves",
+                    password = "3",
+                    rol = "asesor",
+                },
+                new Usuario
+                {
+                    idUsuario = "4",
+                    nombre = "Maritza",
+                    password = "3",
+                    rol = "administrador",
+                },
+            };
+            return list;
+        }
+    }
+}
+                 
